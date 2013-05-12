4e5da8ed4b30e41ee72721a0cad4c3ce3b65dfe7
boardstretcher <boardstretcher@gmail.com>
2013-05-12 15:15:52 -0400

    modified:   functions.sh

--- functions.sh
+++ functions.sh
@@ -1,4 +1,17 @@
+# debug info to be shown if DEBUG is set to 'on'
 function debug()
 {
-	 [ "$DEBUG" == "on" ] &&  echo $1 #$@
+
+	local msg = "[debug] - " + $1
+	[ "$DEBUG" == "on" ] &&  echo $1
+	echo $msg > $LOGFILE
  }
+
+# post info to log
+function info()
+{
+	local msg = "[info] - " + $1
+	[ "$INFO" == "on" ] && echo $msg
+	echo $msg > $LOGFILE
+}
+
