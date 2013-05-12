91f0323d9f30d104892b08175ff664a8e0ae90d6
boardstretcher <boardstretcher@gmail.com>
2013-05-12 17:19:49 -0400

    updates

--- functions.sh
+++ functions.sh
@@ -1,17 +1,87 @@
 # debug info to be shown if DEBUG is set to 'on'
-function debug()
-{
+# usage: debug "text to output in case debug is on"
+function failed() {
+	local r=$?
+	set +o errtrace
+	set +o xtrace
+	echo "An error occurred..."
+	clean_exit
+}
 
-	local msg = "[debug] - " + $1
-	[ "$DEBUG" == "on" ] &&  echo $1
-	echo $msg > $LOGFILE
+# run a cleanup before exiting
+function clean_exit(){
+	debug "Starting cleanup..."
+	rm -f $TMPFILE
+	debug "Finished cleanup"
+	exit
+}
+
+function debug(){
+	local msg="[debug] - $1"
+	[ "$DEBUG" == "on" ] && echo $1
+	[ "$DEBUG" == "on" ] && echo $msg > $LOGFILE
+	return
  }
 
-# post info to log
-function info()
-{
-	local msg = "[info] - " + $1
+# post info to screen and log if INFO is set to on
+# usage: info "text to output in case INFO is on"
+function info(){
+	local msg="[info] - $1"
 	[ "$INFO" == "on" ] && echo $msg
-	echo $msg > $LOGFILE
+	[ "$INFO" == "on" ] && echo $msg > $LOGFILE
+	return
+}
+
+# run multiple commands on a number of hosts using ssh key
+# usage: run_on_hosts(key, hostlist)
+function run_on_hosts(){
+	local key=$1
+	local hostlist=$2
+	local commandlist=$3
+	for hostname in $hostlist
+	do
+		info "working on $hostname"
+		# ssh -i "$key" "$hostname" "$commandlist"
+		echo "$key $hostname $commandlist"
+	done
+	return
+}
+
+# change field seperator
+function change_ifs(){
+	new_ifs=$1
+	OLDIFS="${IFS}"
+	IFS=$new_ifs
+	return
+}
+
+# return true if file exists
+function if_exist(){
+	if [ -f "/some/file/exists" ]; then
+	echo "the file exists"
+	fi
+}
+
+# look for a regex in a string, if match return true
+function check_regex(){
+	local input=$1
+	local regex=$2
+	if [[ $input =~ $regex ]]; then
+		# echo "found some regex"
+		return true
+	else
+		# echo "did not find some regex"
+		return false
+	fi
+}
+
+# show script usage and exit
+function usage(){
+	source usage.dat
+}
+
+# show mini script usage and exit
+function mini_usage(){
+	source mini_usage.dat
 }
 
