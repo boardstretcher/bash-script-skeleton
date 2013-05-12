35e50d61f278d3dc02169d276125a6cdc1de5896
boardstretcher <boardstretcher@gmail.com>
2013-05-12 15:16:17 -0400

    modified:   wireframe.sh

--- wireframe.sh
+++ wireframe.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
+# bash administration framework v16
+# by: steve zornes
+# 
+
 # import custom functions
 source functions.sh
 
 # debugging on or off
 DEBUG=on
 
+# informational output on or off
+INFO=on
+
 # create a tmp file
 TMPFILE=$(mktemp /tmp/myfile.XXXXX)
+
+# set log file
+LOGFILE=/var/log/someapp.log
 
 # fail and exit on first error
 set -o errtrace
@@ -25,6 +35,7 @@ failed() {
 	exit $r
 }
 
+# example debug output
 debug "debug info if DEBUG=on is set at beginning of script"
 
 # show all output to console
